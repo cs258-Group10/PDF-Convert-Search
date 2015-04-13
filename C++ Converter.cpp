c8af1f5b3492b7f739165215dd01bc30c1f15732
UTKARSH SAXENA <usaxena95@gmail.com>
2015-04-13 19:46:42 +0530

    All three type of files are created !!

--- C++ Converter.cpp
+++ C++ Converter.cpp
@@ -1,40 +1,13 @@
 #include<bits/stdc++.h>
 #include<iostream>
+#include <string>
+#define ps push_back
 using namespace std;
 #define fre 	freopen("0.in","r",stdin);freopen("WITH_ABS.csv","w",stdout)
-#define abs(x) ((x)>0?(x):-(x))
-#define M 1000000007
-#define lld signed long long int
-#define pp pop_back()
-#define ps(x) push_back(x)
-#define mpa make_pair
-#define pii pair<int,int>
-#define fi first
-#define se second
-#define scan(x) scanf("%d",&x)
-#define print(x) printf("%d\n",x)
-#define scanll(x) scanf("%lld",&x)
-#define printll(x) printf("%lld\n",x)
-#define boost ios_base::sync_with_stdio(0)
+
 //vector<int> g[2*100000+5];int par[2*100000+5];
-
-vector<string> token(string S)
-{
-	vector<string>ans;
-	ans.clear();
-
-	for(int i=0;i<S.size();)
-	{
-		string temp="";
-
-		while(S[i]!=' ')
-			temp+=S[i],++i;
-		++i;
-		ans.ps(temp);
-
-	}
-	return ans;
-}
+vector<string> sentence;
+int AT;
 
 string purify(string S)
 {
@@ -96,11 +69,165 @@ string  abstract(string start_word, string end_word,string e2)
 	cout<<"\",";
 	return start_word;
 }
-
-int main()
+string to_string(int x)
 {
-	freopen("0.in","r",stdin);
-	freopen("WITH_ABS.csv","w",stdout);
+	string S="";
+	while(x>0)
+	{
+		S=(char)('0'+x%10)+S;
+		x/=10;
+	}
+	return S;
+}
+
+vector<string> token(string S)
+{
+	vector<string>ans;
+	ans.clear();
+
+	for(int i=0;i<S.size();)
+	{
+		string temp="";
+
+		while(S[i]!=' ' and i<S.size())
+			temp+=S[i],++i;
+		++i;
+		if(temp!="")
+			ans.ps(temp);
+
+		while(S[i]==' ' and i<S.size())
+			++i;
+	}
+	return ans;
+}
+
+bool is_a_date(string S)
+{
+	if(S.size()!=10)
+		return 0;
+	if(S[2]==S[5] and S[5]=='/')
+	{
+		return 1;
+	}
+	else return 0;
+
+}
+
+int get_next(string &S)
+{
+	if(AT>=sentence.size())
+	{
+		while(std::getline (std::cin,S))
+		{
+			sentence=token(S);
+			if(sentence.size()==0)
+				continue;
+			if(S.find("Publication Under Section 43(2) in Respect of the Grant")!=string::npos)
+			{
+				return 2;
+				AT=100000000;
+			}
+			AT=1;
+			S=sentence[0];
+			return 1;
+		}
+		return 0;
+	}
+	else
+	{
+		S=sentence[AT];
+		AT++;
+		return 1;
+	}
+}
+void GRANTED_data_incoming()
+{
+	string HEAD="";
+	HEAD+="Patent Number,Application No.,Date of Application,Date of Priority,Title of Invention AND Name of patentee,Date of Publication of Abstract,Appropriate Office\n";
+	cout<<HEAD;
+	string I,S;
+	AT=100000000;
+	int DEC,flag=0;
+	for(int i=1;get_next(S);++i)
+	{
+		I=to_string(i);
+		while(1)
+		{
+			if(S==I)
+				break;
+			else
+
+			DEC=get_next(S);
+
+			if(DEC==0)
+				return;
+			else
+			if(flag==0 and DEC==2)
+			{
+				flag=1;
+				i=1;
+				I=to_string(i);
+			}
+		}
+
+
+		get_next(S);					//PatentNumber
+		cout<<"\""<<S<<"\",";
+
+		get_next(S);					//Application Number
+		cout<<"\""<<S<<"\",";
+
+		get_next(S);					//Date of Application
+
+		if(!is_a_date(S))
+		{
+			S="";
+			AT--;
+		}
+		cout<<"\""<<S<<"\",";
+
+		get_next(S);					//Date of Priority
+		if(!is_a_date(S))
+		{
+			S="";
+			AT--;
+		}
+		cout<<"\""<<S<<"\",";
+
+		S=purify(S);
+		cout<<"\"";
+			//cout<<"\nTitle of Invention 		and			 	Name of Patentee\n";
+		while(get_next(S))
+		{
+			S=purify(S);
+			if(is_a_date(S))
+				break;
+			cout<<S<<' ';
+		}
+		cout<<"\",";
+
+		cout<<"\""<<S<<"\","; 		//Date of Publication of Abstract u/s 11(A)
+
+		get_next(S);					//Appropriation Office
+		cout<<"\""<<S<<"\",";
+		cout<<endl;
+	}
+}
+int main(int argc, char *argv[])
+{
+	char *I1=" Early Publication.csv";
+	char *I2=" Publication After 18 Months.csv";
+	char *I3=" GRANTED.csv";
+
+
+	freopen(argv[1],"r",stdin);
+
+	char *OUTPUT_1=new char[strlen(argv[1])+strlen(I1)+1];
+	strcpy(OUTPUT_1,argv[1]);
+	strcat(OUTPUT_1,I1);
+
+	freopen(OUTPUT_1,"w",stdout);
+
 
 	string start="(12) PATENT APPLICATION PUBLICATION";
 	//string end_string="(57) Abstract : ";
@@ -112,18 +239,45 @@ int main()
 	c1+="\"INTERNATIONAL PUBLICATION NUMBER\",\"Patent of Addition to Application Number\",\"Patent of Addition to Application Number FILLING DATE\",\"DIVISIONAL TO APPLICATION NUMBER\",\"DIVISIONAL TO APPLICATION NUMBER FILLING DATE\"";
 	c1+=",\"DETAIL OF APPLICANT\",\"DETAIL OF INVENTOR\",\"ABSTRACT\"";
 	cout<<c1<<endl;
+	int Pub_after_18_open=0;
 	while(std::getline (std::cin,S))
 	{
 		S=purify(S);
+
+		if(S.find("Publication Under Section 43(2) in Respect of the Grant")!=string::npos)
+		{
+			fclose (stdout);
+
+			char *OUTPUT_3=new char[strlen(argv[1])+strlen(I3)+1];
+			strcpy(OUTPUT_3,argv[1]);
+			strcat(OUTPUT_3,I3);
+			freopen(OUTPUT_3,"w",stdout);
+
+			GRANTED_data_incoming();
+			return 0;
+		}
+		if(S.find("Publication After 18 Months :")!=string::npos and Pub_after_18_open==0)
+		{
+			Pub_after_18_open=1;
+			fclose (stdout);
+
+			char *OUTPUT_2=new char[strlen(argv[1])+strlen(I2)+1];
+			strcpy(OUTPUT_2,argv[1]);
+			strcat(OUTPUT_2,I2);
+			freopen(OUTPUT_2,"w",stdout);
+
+		}
 		if(S.size()>=start.size() and S.substr(0,start.size())==start)
 		{
 			//dealing with first three lines
+
 			/*
 			(12) PATENT APPLICATION PUBLICATION (21) Application No.329/CHE/2009 A
 			(19) INDIA
 			(22) Date of filing of Application :14/02/2009 (43) Publication Date : 03/04/2009
 			(54) Title of the invention : Using rack and pinions to drive the power output shaft in an internal combustion engine
 			*/
+
 			cout<<"\""<<S.substr(S.find('.')+1)<<"\",";	//printing data after '.' (i.e. application number)
 			std::getline (std::cin,S);//skip (19)india
 			std::getline (std::cin,S);//now S contains the third Line
@@ -252,5 +406,6 @@ int main()
 			//return 0;
 		}
 	}
+	remove(argv[1]);
 	return 0;
 }
